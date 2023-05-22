59dc8f141291f8ab4629e2b8c54fed25acfecbd0
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-22 20:02:33 +0000

    ♻️  Refactor. This is the way.

--- python_katas/high_and_low.py
+++ python_katas/high_and_low.py
@@ -1,5 +1,5 @@
-def high_and_low(numbers):
-    num_ls = numbers.split()
+def high_and_low(numbers_str):
+    num_ls = numbers_str.split()
     numbers = [int(num) for num in num_ls]
     max_v = max(numbers)
     min_v = min(numbers)
