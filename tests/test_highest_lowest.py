c38332f2cbcac7c1e9cc6aa1a0c1194210a53238
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-22 19:40:43 +0000

    🛑🧪 Fail tests. This is the way.

--- tests/test_highest_lowest.py
+++ tests/test_highest_lowest.py
@@ -5,3 +5,6 @@ def test_high_and_low():
     expected = "2 1"
     obtained = pk.high_and_low("2 1")
     assert obtained == expected
+    expected = "3 2"
+    obtained = pk.high_and_low("3 2")
+    assert obtained == expected
