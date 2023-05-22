d8dc8ffc347f91f641c3774a6d094dc997d5ebaf
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-22 19:59:23 +0000

    ✅ Pass tests. This is the way.

--- python_katas/high_and_low.py
+++ python_katas/high_and_low.py
@@ -1,2 +1,7 @@
 def high_and_low(numbers):
+    num_ls = numbers.split()
+    numbers = [int(num) for num in num_ls]
+    max_v = max(numbers)
+    min_v = min(numbers)
+    numbers = f"{max_v} {min_v}"
     return numbers
