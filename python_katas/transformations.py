476501316e4aa90281ca33cce85e350c82f64bef
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-22 12:30:35 -0700

    🚚 Remove old tests and functions

--- python_katas/transformations.py
+++
@@ -1,2 +0,0 @@
-def add_offset(augend: int, addend: int) -> int:
-    return augend + addend
--- tests/test_transformations.py
+++
@@ -1,9 +0,0 @@
-import python_katas as pk
-
-
-def test_add_offset():
-    augend = 1
-    addend = 2
-    expected = augend + addend
-    obtained = pk.add_offset(augend, addend)
-    assert expected == obtained
