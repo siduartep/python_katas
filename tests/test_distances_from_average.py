c34491525eb3d946df52eb11d757f147bcd8684b
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-24 18:58:02 +0000

    🛑🧪 Fail tests. This is the way.

--- tests/test_distances_from_average.py
+++ tests/test_distances_from_average.py
@@ -8,3 +8,6 @@ def test_distances_from_average():
     expected = [0]
     obtained = pk.distances_from_average([1])
     assert obtained == expected
+    expected = [0, 0]
+    obtained = pk.distances_from_average([2, 2])
+    assert obtained == expected
