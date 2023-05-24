7432063587d47c99360dcc977abdc95c659fdfed
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-24 17:21:09 +0000

    🛑🧪 Fail tests. This is the way.

--- tests/test_distances_from_average.py
+++ tests/test_distances_from_average.py
@@ -2,4 +2,6 @@ import python_katas as pk
 
 
 def test_distances_from_average():
-    pk.distances_from_average()
+    obtained = [1, 2, 3]
+    expected = pk.distances_from_average([1, 2, 3])
+    assert obtained == expected
