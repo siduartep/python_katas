8bb5bf335259b48e9d4cb892bf9936ad18b7fbce
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-26 16:44:23 +0000

    ✅ Pass tests. This is the way.

--- tests/test_distances_from_average.py
+++ tests/test_distances_from_average.py
@@ -8,6 +8,3 @@ def test_distances_from_average():
     expected = [0]
     obtained = pk.distances_from_average([1])
     assert obtained == expected
-    expected = [0, 0]
-    obtained = pk.distances_from_average([2, 2])
-    assert obtained == expected
