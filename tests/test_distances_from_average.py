56ff79c707731e9ac0b438730fa968180a1e6563
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-26 16:45:13 +0000

    ♻️  Refactor. This is the way.

--- tests/test_distances_from_average.py
+++ tests/test_distances_from_average.py
@@ -2,9 +2,9 @@ import python_katas as pk
 
 
 def test_distances_from_average():
-    expected = [0]
     obtained = pk.distances_from_average([0])
-    assert obtained == expected
     expected = [0]
+    assert obtained == expected
     obtained = pk.distances_from_average([1])
+    expected = [0]
     assert obtained == expected
