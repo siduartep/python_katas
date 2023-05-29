ea819dd42c4ccf5c2fb1de52388679c8c758dfd5
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-29 16:42:01 +0000

    ✅ Pass tests. This is the way.

--- python_katas/distances_from_average.py
+++ python_katas/distances_from_average.py
@@ -1,6 +1,6 @@
 def distances_from_average(data):
     average = get_average(data)
-    return [element - average[0] for element in data]
+    return [average[0] - element for element in data]
 
 
 def get_average(numbers):
