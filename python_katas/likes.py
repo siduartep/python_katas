e2a7d875aa33c7311df4d33513e13a6d8c035afe
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-31 17:55:41 +0000

    ✅ Pass tests. This is the way.

--- python_katas/likes.py
+++ python_katas/likes.py
@@ -1,5 +1,8 @@
 def likes(name):
+    number_likes = len(name)
     if name == []:
         return "no one likes this"
-    else:
+    elif number_likes == 1:
         return "Peter likes this"
+    else:
+        return "Jacob and Alex like this"
