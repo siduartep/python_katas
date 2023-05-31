34a6e675480a5e0402829a7061577034d2be75a1
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-31 18:37:23 +0000

    ♻️  Refactor. This is the way.

--- python_katas/likes.py
+++ python_katas/likes.py
@@ -1,9 +1,10 @@
 def likes(name):
     number_likes = len(name)
-    is_one_person = number_likes == 0
-    if is_one_person:
+    is_no_one = number_likes == 0
+    is_one_person = number_likes == 1
+    if is_no_one:
         return print_no_one(name)
-    elif number_likes == 1:
+    elif is_one_person:
         return print_one_person(name)
     elif number_likes == 2:
         return print_two_people(name)
