b55025168b09e536b98a171b1a8711bc5a393c2d
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-31 17:06:13 +0000

    ✅ Pass tests. This is the way.

--- python_katas/__init__.py
+++ python_katas/__init__.py
@@ -1,5 +1,6 @@
 """A template Python module"""
 
 __version__ = "0.4.0"
-from .high_and_low import high_and_low
+from .high_and_low import *
 from .distances_from_average import *
+from .likes import *
