b38690b7c023a8aa99e9ff393cb80455efd9fa32
Simon Duarte <simon.duarte@islas.org.mx>
2023-05-31 19:34:10 +0000

    ✅ Pass tests. This is the way.

--- python_katas/likes.py
+++ python_katas/likes.py
@@ -3,6 +3,12 @@ def likes(name):
     return printer[number_likes](name)
 
 
+def print_four_or_more_people(name):
+    number_likes = len(name)
+    others = number_likes - 2
+    return f"{name[0]}, {name[1]} and {others} others like this"
+
+
 def print_three_people(name):
     return f"{name[0]}, {name[1]} and {name[2]} like this"
 
@@ -19,4 +25,10 @@ def print_no_one(name):
     return "no one likes this"
 
 
-printer = [print_no_one, print_one_person, print_two_people, print_three_people]
+printer = [
+    print_no_one,
+    print_one_person,
+    print_two_people,
+    print_three_people,
+    print_four_or_more_people,
+]
